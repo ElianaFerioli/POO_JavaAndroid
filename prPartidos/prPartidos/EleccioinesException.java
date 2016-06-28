67e5e6c7f2fc740483143dc66a8913cd4101ebf9
Eliana Ferioli <elianaferioli@gmail.com>
2016-06-28 11:49:58 +0200

    Add files via upload

---
+++ prPartidos/prPartidos/EleccioinesException.java
@@ -0,0 +1,13 @@
+package prPartidos;
+
+/**
+ * Created by SamsungAndroid on 28/06/2016.
+ */
+public class EleccioinesException extends RuntimeException{
+    public EleccioinesException() {
+    }
+
+    public EleccioinesException(String mensaje) {
+        super(mensaje);
+    }
+}
---
+++ prPartidos/prPartidos/Elecciones.java
@@ -0,0 +1,67 @@
+package prPartidos;
+
+import java.util.*;
+
+/**
+ * Created by SamsungAndroid on 28/06/2016.
+ */
+public class Elecciones {
+    //Atributos de instancia
+    protected List<Partido> partidos;
+    //Constructor
+    public Elecciones(String[] datos){
+        partidos = new ArrayList<Partido>();
+        for(String dato : datos){
+            Partido p = stringAPartido(dato);
+            partidos.add(p);
+        }
+    }
+    //Métodos
+    private Partido stringAPartido(String dato){
+        try(Scanner sc = new Scanner(dato)){
+            sc.useDelimiter("[,]+");
+            String nombrePartido = sc.next();
+            int votos = sc.nextInt();
+            return new Partido(nombrePartido, votos);
+        } catch (InputMismatchException e) {
+            throw new EleccioinesException("Votos no numérico en " + dato);
+        } catch (NoSuchElementException e) {
+            throw new EleccioinesException("Faltan datos en " + dato);
+        }
+    }
+    protected Set<Token> creaTokens(int numEsc){
+        Set<Token> set = new TreeSet<>();
+        for(int i = 0; i < partidos.size(); i++){
+            for(int n = 1; n <= numEsc; n++){
+                set.add(new Token(partidos.get(i), n));
+            }
+        }
+        return set;
+    }
+    private Set<Token> seleccionaTokens(Set<Token> set, int numEsc){
+        Set<Token> nSet = new TreeSet<>();
+        Iterator<Token> it = set.iterator();
+        for(int i = 0; i < numEsc; i++){
+            nSet.add(it.next());
+        }
+        return nSet;
+    }
+    public void generaResultados(int numEsc){
+        Set<Token> setTodos = creaTokens(numEsc);
+        Set<Token> setMenores = seleccionaTokens(setTodos, numEsc);
+        for(Partido p : partidos){
+            int nv = 0;
+            for(Token tk: setMenores){
+                if(p.equals(tk.getPartido())){
+                    nv++;
+                }
+            }
+            p.setNumEsc(nv);
+        }
+    }
+    public void muestraResultados(){
+        for(Partido p: partidos){
+            System.out.println(p);
+        }
+    }
+}
